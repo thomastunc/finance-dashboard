436c1faeae8328e57d7d81285127616f49003048
Thomas Tunc <28352452+thomastunc@users.noreply.github.com>
2024-10-04 18:25:11 +0200

    Upgrade degiro-connector to 3.x

--- src/model/stock/degiro.py
+++ src/model/stock/degiro.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-from degiro_connector.core.helpers import pb_handler
 from degiro_connector.trading.api import API
-from degiro_connector.trading.models.trading_pb2 import Credentials, Update, ProductsInfo
+from degiro_connector.trading.models.account import UpdateRequest, UpdateOption
+from degiro_connector.trading.models.credentials import Credentials
 
 
 class DeGiro:
@@ -15,65 +15,72 @@ class DeGiro:
         ))
         self.trading_api.connect()
 
-    def search_stock(self, index_id: str):
-        request = ProductsInfo.Request()
-        request.products.extend([int(index_id)])
-
+    def search_stock(self, product_id: str):
         products_info = self.trading_api.get_products_info(
-            request=request,
+            product_list=[product_id],
             raw=True,
         )
-
         return products_info
 
     def retrieve_stocks(self):
-        request_list = Update.RequestList()
-        request_list.values.extend([Update.Request(option=Update.Option.PORTFOLIO, last_updated=0)])
+        update = self.trading_api.get_update(
+            request_list=[
+                UpdateRequest(
+                    option=UpdateOption.PORTFOLIO,
+                    last_updated=0,
+                ),
+            ],
+            raw=True
+        )
 
-        update = self.trading_api.get_update(request_list=request_list, raw=False)
-        update_dict = pb_handler.message_to_dict(message=update)
-
-        portfolio_df = pd.DataFrame(update_dict["portfolio"]["values"])
-        portfolio_df = portfolio_df[portfolio_df['positionType'] == 'PRODUCT']
-        portfolio_df = portfolio_df[portfolio_df['size'] > 0]
-
+        portfolio_df = pd.DataFrame(update["portfolio"]["value"])
         rows = []
 
         for item in portfolio_df.index:
-            product_id = portfolio_df['id'][item]
-            stock_metadata = self.search_stock(product_id)
+            item_metadata = {entry['name']: entry.get('value', None) for entry in portfolio_df["value"][item]}
+            item_amount = int(item_metadata['size'])
 
-            currency = stock_metadata['data'][product_id]['currency']
-            purchase_value = portfolio_df['breakEvenPrice'][item]
-            current_value = portfolio_df['price'][item]
-            portfolio_value = portfolio_df['value'][item]
+            if item_metadata['positionType'] == 'PRODUCT' and item_amount > 0:
+                product_id = portfolio_df['id'][item]
+                stock_metadata = self.search_stock(product_id)
 
-            rows.append({
-                "name": stock_metadata['data'][product_id]['name'],
-                "symbol": stock_metadata['data'][product_id]['symbol'],
-                "amount": int(portfolio_df['size'][item]),
-                "purchase_value": purchase_value,
-                "current_value": current_value,
-                "portfolio_value": round(portfolio_value, 2),
-                "currency": currency
-            })
+                currency = stock_metadata['data'][product_id]['currency']
+                purchase_value = item_metadata['breakEvenPrice']
+                current_value = item_metadata['price']
+                portfolio_value = item_metadata['value']
+
+                rows.append({
+                    "name": stock_metadata['data'][product_id]['name'],
+                    "symbol": stock_metadata['data'][product_id]['symbol'],
+                    "amount": item_amount,
+                    "purchase_value": purchase_value,
+                    "current_value": current_value,
+                    "portfolio_value": round(portfolio_value, 2),
+                    "currency": currency
+                })
 
         return pd.DataFrame(rows)
 
     def retrieve_account(self):
-        request_list = Update.RequestList()
-        request_list.values.extend([Update.Request(option=Update.Option.TOTALPORTFOLIO, last_updated=0)])
-
-        update = self.trading_api.get_update(request_list=request_list, raw=False)
-        update_dict = pb_handler.message_to_dict(message=update)
+        update = self.trading_api.get_update(
+            request_list=[
+                UpdateRequest(
+                    option=UpdateOption.TOTAL_PORTFOLIO,
+                    last_updated=0,
+                ),
+            ],
+            raw=True
+        )
 
         client_details_table = self.trading_api.get_client_details()
+        total_portfolio_df = pd.DataFrame(update["totalPortfolio"])
+        total_portfolio_metadata = {entry['name']: entry.get('value', None) for entry in total_portfolio_df["value"]}
 
         rows = [{
             "name": "Flatex",
             "iban": client_details_table['data']['flatexBankAccount']['iban'],
-            "balance": float(update_dict['total_portfolio']['values']['totalCash']),
-            "currency": update_dict['total_portfolio']['values']['cashFundCompensationCurrency']
+            "balance": total_portfolio_metadata["totalCash"],
+            "currency": total_portfolio_metadata["cashFundCompensationCurrency"]
         }]
 
         return pd.DataFrame(rows)
